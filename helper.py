17d4e9d632e679316ea44aa95b4a56a582d5a0bb
nhi-batau <ssmemes163@gmail.com>
2025-04-08 01:54:26 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -300,7 +300,7 @@ async def download_video(url,cmd, name):
         return os.path.isfile.splitext[0] + "." + "mp4"   
    
 async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):   
-    reply = await m.reply_text(f"Uploading - `{name}`")   
+    reply = await m.reply_text(f"Uploading - `{name}`\n\n BOT MADE BY - 🅱︎🅷︎🆄︎🅼︎🅸︎🅷︎🅰︎🆁︎")   
     time.sleep(1)   
     start_time = time.time()   
     await m.reply_document(ka,caption=cc1)   
@@ -379,7 +379,7 @@ async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     emoji = get_next_emoji()
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:00:02 -vframes 1 "{filename}.jpg"', shell=True)   
     await prog.delete (True)   
-    reply = await m.reply_text(f"**Uploading ...** - `{name}`")   
+    reply = await m.reply_text(f"**Uploading ...** - `{name}`\n\n BOT MADE BY - 🅱︎🅷︎🆄︎🅼︎🅸︎🅷︎🅰︎🆁︎")   
     try:   
         if thumb == "no":   
             thumbnail = f"{filename}.jpg"   
